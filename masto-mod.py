6aca247afabe560f150ec8e9e37d24dfbfd8f809
catgoat <mckatze@gmail.com>
2019-07-04 21:55:00 -0400

    setup! and start on the reports module

--- masto-mod.py
+++ masto-mod.py
@@ -5,7 +5,10 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from mastodon import Mastodon, StreamListener
 from listeners.public import PublicStreamListener
 from rules.status import StatusRules
+from reports.reports import Reports
 import yaml
+import argparse
+import getpass
 
 
 # Enable logging
@@ -23,16 +26,18 @@ def error(update,context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def setup(config):
-    """One time setup for the bot -- creates the api key and secrets you'll need to hit yr"""
-    print("Enter your bot's mastodon login name")
+    """One time setup for the bot -- creates the api key and secrets you'll need to hit yr instance"""
+    print("Enter your bot's mastodon login:")
     username = input()
-    print("Enter your mastodon password")
-    password = input()
+    print("Enter your bot's mastodon password")
+    password = getpass.getpass()
     print("Creating app on your server....")
     Mastodon.create_app(
         config['server']['app_name'],
         api_base_url = config['server']['url'],
-        to_file = 'pytooter_clientcred.secret'
+        to_file = 'pytooter_clientcred.secret',
+        scopes= ['admin:read', 'read']
+
     )
     print("app created, logging in...")
     mastodon = Mastodon(
@@ -42,15 +47,24 @@ def setup(config):
     mastodon.log_in(
         username,
         password,
-        to_file = 'pytooter_usercred.secret'
+        to_file = 'pytooter_usercred.secret',
+        scopes=['admin:read','read']
     )
     print("Ok thnx, saved credentials!")
 
 def main():
+
+    parser = argparse.ArgumentParser(description="Telegram mastodon mod bot")
+    parser.add_argument("--setup", help="Initial setup of the bot, creates the app on the instance and a login token", action="store_true")
+    args = parser.parse_args()
+   
     #open the settings file
     with open("settings.yml") as settings:
         config = yaml.safe_load(settings)
-
+    
+    if args.setup:
+        setup(config)
+        exit()
     #start masotdon.py client
     mastodon = Mastodon(
         client_id = 'pytooter_clientcred.secret',
@@ -77,7 +91,17 @@ def main():
         rules =  rules
     )
 
+    reports = Reports(
+        mastodon = mastodon,
+        config = config['reports'],
+        bot = updater.bot,
+        chat_id = config['chat_id']
+    )
+
+    reports.start_monitoring()
+
     mastodon.stream_public(listener)
+    
     updater.idle()
    
 
--- reports/reports.py
+++ reports/reports.py
@@ -1,12 +1,22 @@
-import sched
+import schedule
+import time
+from mastodon import Mastodon
 
-class ReportsWatcher():
+class Reports():
     """Kicks off a scheduled task to poll the reports and ping the bot if there's new reports"""
-    def __init__(self, mastodon, config, telegram):
+    def __init__(self, mastodon: Mastodon, config, bot, chat_id):
         self.mastodon = mastodon
         self.config = config
-        self.telegram = telegram
-        s = sched.scheduler()
-        
+        self.bot = bot
+        self.chat_id = chat_id
+
     def check_report_queue(self):
-        pass
+        reports = self.mastodon.admin_reports()
+        print(reports)
+        self.bot.send_message(self.chat_id, f'{len(reports)} in the report queue!')
+        
+    def start_monitoring(self):
+         schedule.every().minute.do(self.check_report_queue)
+         while True:
+            schedule.run_pending()
+            time.sleep(1)
